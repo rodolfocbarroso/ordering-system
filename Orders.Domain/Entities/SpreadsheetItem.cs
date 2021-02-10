259eaa3545b0d5d16cbe75b95aab73854c792ddf
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-09 23:11:40 -0300

    ✨ Adiciona a classe SpreadsheetItem

---
+++ Orders.Domain/Entities/SpreadsheetItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Flunt.Validations;
+using Orders.Domain.Entities.Base;
+
+namespace Orders.Domain.Entities
+{
+    public class SpreadsheetItem : Entity
+    {
+        public SpreadsheetItem(long line, string name, DateTime deliveryDate, int quantity, decimal unitPrice)
+        {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .IsLowerOrEqualsThan(line, 0, "Line", "A linha inválida.")
+                    .IsNotNullOrEmpty(name, "Name", "O nome não pode ser vazio.")
+                    .HasMaxLen(name, 50, "Name", "O nome não pode ter tamanho maior que 50 caracteres.")
+                    .IsNotNull(deliveryDate, "DeliveryDate", "A data de entrega não pode ser vazia.")
+                    .IsLowerOrEqualsThan(deliveryDate, DateTime.Today, "DeliveryDate", "A data de entrega não pode ser menor ou igual ao dia de hoje.")
+                    .IsLowerOrEqualsThan(quantity, 0, "Quantity", "A quantidade não pode ser menor ou igual a zero.")
+                    .IsLowerOrEqualsThan(unitPrice, 0, "UnitPrice", "O preço unitário não pode menor ou igual a zero.")
+            );
+
+            Line = line;
+            Name = name;
+            DeliveryDate = deliveryDate;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+        public long Line { get; private set; }
+        public string Name { get; private set; }
+        public DateTime DeliveryDate { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+    }
+}
