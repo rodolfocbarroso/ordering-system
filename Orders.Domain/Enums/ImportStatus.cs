36a8aeb332fb38344c7a6129759a1531489468d0
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-10 00:12:32 -0300

    ✨ Adiciona enum ImportStatus

---
+++ Orders.Domain/Enums/ImportStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orders.Domain.Enums
+{
+    public enum ImportStatus
+    {
+        Success = 0,
+        Error = 1,
+        Pending = 2
+    }
+}
