c038b58e3639debc64fce6b02b5de748ecbb5d81
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-10 00:13:43 -0300

    ✨ Adiciona classe import

---
+++ Orders.Domain/Entities/Base/Import.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Orders.Domain.Entities.Base;
+using Orders.Domain.Enums;
+
+namespace Orders.Domain.Entities.Base
+{
+    public abstract class Import : Entity
+    {
+        protected Import(FileExtension fileExtension, ImportStatus importStatus)
+        {
+            Instant = DateTime.Now;
+            FileType = fileExtension;
+            ImportStatus = importStatus;
+        }
+
+        public DateTime Instant { get; private set; }
+        public ImportStatus ImportStatus { get; private set; }
+        public FileExtension FileType { get; private set; }
+    }
+}
