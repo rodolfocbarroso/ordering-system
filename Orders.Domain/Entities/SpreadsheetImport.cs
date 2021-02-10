94cec44625359a3504530e2b848f15d2e2042102
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-10 00:14:49 -0300

    ✨ Adiciona classe SpreadsheetImport

---
+++ Orders.Domain/Entities/SpreadsheetImport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Orders.Domain.Entities.Base;
+using Orders.Domain.Enums;
+
+namespace Orders.Domain.Entities
+{
+    public class SpreadsheetImport : Import
+    {
+
+        public SpreadsheetImport(FileExtension fileExtension, ImportStatus importStatus) : base(fileExtension, importStatus)
+        {
+            Items = new List<SpreadsheetItem>();
+        }
+
+
+        public IList<SpreadsheetItem> Items { get; private set; }
+
+        public void AddItem(long line, string name, DateTime deliveryDate, int quantity, decimal unitPrice)
+        {
+            var item = new SpreadsheetItem(line, name, deliveryDate, quantity, unitPrice);
+            if (item.Valid)
+                Items.Add(item);
+        }
+    }
+}
