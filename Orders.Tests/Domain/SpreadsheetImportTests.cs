aa23c300c9a1fbdf290763266def180b7610bee5
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-10 00:51:50 -0300

    🎨 Adiciona projeto de tests

---
+++ Orders.Tests/Domain/SpreadsheetImportTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orders.Domain.Entities;
+using Orders.Domain.Enums;
+
+namespace Orders.Tests.Domain
+{
+    [TestClass]
+    public class SpreadsheetImportTests
+    {
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_arquivo_ele_deve_gerar_uma_importacao()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Orders.Tests/Domain/SpreadsheetItemTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orders.Domain.Entities;
+using Orders.Domain.Enums;
+
+namespace Orders.Tests.Domain
+{
+    [TestClass]
+    public class SpreadsheetItemTests
+    {
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_item_com_a_linha_invalida_o_mesmo_nao_pode_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_item_com_o_nome_maior_que_50_caracteres_o_mesmo_nao_pode_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+    }
+}
