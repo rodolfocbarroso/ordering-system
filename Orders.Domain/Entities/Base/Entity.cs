27256dc5afad8dd491b213157d376d48ba227a95
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-09 21:23:59 -0300

    ✨ Adiciona entidade base

---
+++ Orders.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Flunt.Notifications;
+
+namespace Orders.Domain.Entities.Base
+{
+    public class Entity : Notifiable
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
