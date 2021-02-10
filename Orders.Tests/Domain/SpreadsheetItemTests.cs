e06f7fd0a1fde3ed7c13e8faecb25893a31f4ff0
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-10 01:25:51 -0300

    ✅ Adiciona testes de SpreadsheetItem

--- Orders.Tests/Domain/SpreadsheetItemTests.cs
+++ Orders.Tests/Domain/SpreadsheetItemTests.cs
@@ -12,7 +12,15 @@ namespace Orders.Tests.Domain
     {
         [TestMethod]
         [TestCategory("Domain")]
-        public void Dado_um_item_com_a_linha_invalida_o_mesmo_nao_pode_ser_adicionado()
+        public void Dado_um_item_com_a_linha_igual_a_zero_o_mesmo_nao_pode_ser_adicionado()
+        {
+            var item = new SpreadsheetItem(0, "produto 1", DateTime.Now.AddDays(2), 2, 2000);
+            Assert.AreNotEqual(0, item.Line);
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_item_com_o_nome_vazio_o_mesmo_nao_pode_ser_adicionado()
         {
             Assert.Fail();
         }
@@ -20,6 +28,34 @@ namespace Orders.Tests.Domain
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_item_com_o_nome_maior_que_50_caracteres_o_mesmo_nao_pode_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_item_com_uma_data_de_entrega_vazia_o_mesmo_nao_pode_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_item_com_uma_data_de_entrega_menor_ou_igual_ao_dia_de_hoje_o_mesmo_nao_pode_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_item_com_uma_quantidade_menor_ou_igual_a_zero_o_mesmo_nao_pode_ser_adicionado()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Dado_um_item_com_um_preco_unitario_menor_ou_igual_a_zero_o_mesmo_nao_pode_ser_adicionado()
         {
             Assert.Fail();
         }
