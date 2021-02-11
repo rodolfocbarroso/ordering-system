f57e14eea987ac8f9d5ab7599fcf552da3b11926
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-10 23:54:28 -0300

    🔨 Refatora classes de teste

---
+++ Orders.Tests/Domain/ImportItemTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orders.Domain.Entities;
+using Orders.Domain.Enums;
+
+namespace Orders.Tests.Domain
+{
+    [TestClass]
+    public class ImportItemTests
+    {
+    }
+}
--- Orders.Tests/Domain/ImportTests.cs
+++ Orders.Tests/Domain/ImportTests.cs
@@ -8,13 +8,8 @@ using Orders.Domain.Enums;
 namespace Orders.Tests.Domain
 {
     [TestClass]
-    public class SpreadsheetImportTests
+    public class ImportTests
     {
-        [TestMethod]
-        [TestCategory("Domain")]
-        public void Dado_um_arquivo_ele_deve_gerar_uma_importacao()
-        {
-            Assert.Fail();
-        }
+
     }
 }
