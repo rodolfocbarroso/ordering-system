edae9be6cc86e90376dd2a141c0a92a7c8384fc5
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-11 19:48:55 -0300

    ✨ Adiciona contract do repository de Import

---
+++ Orders.Domain/Repositories/Contracts/IImportRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using Orders.Domain.Entities;
+
+namespace Orders.Domain.Repositories.Contracts
+{
+    public interface IImportRepository
+    {
+        void Save(Import import);
+        Import GetById(Guid id);
+        IEnumerable<Import> GetAll();
+
+    }
+}
