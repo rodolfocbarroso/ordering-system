ab20431ffb52e67fdc80eb0c248b9cadf44d79cb
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-10 23:48:39 -0300

    🔨 Refatora classe de import

--- Orders.Domain/Entities/Base/Import.cs
+++ Orders.Domain/Entities/Base/Import.cs
@@ -6,17 +6,27 @@ using Orders.Domain.Enums;
 
 namespace Orders.Domain.Entities.Base
 {
-    public abstract class Import : Entity
+    public class Import : Entity
     {
-        protected Import(FileExtension fileExtension, ImportStatus importStatus)
+        public Import(FileExtension fileExtension, ImportStatus importStatus)
         {
             Instant = DateTime.Now;
             FileType = fileExtension;
             ImportStatus = importStatus;
+            Items = new List<ImportItem>();
         }
 
         public DateTime Instant { get; private set; }
         public ImportStatus ImportStatus { get; private set; }
         public FileExtension FileType { get; private set; }
+
+        public IList<ImportItem> Items { get; private set; }
+
+        public void AddItem(long line, string name, DateTime deliveryDate, int quantity, decimal unitPrice)
+        {
+            var item = new ImportItem(line, name, deliveryDate, quantity, unitPrice);
+            if (item.Valid)
+                Items.Add(item);
+        }
     }
 }
