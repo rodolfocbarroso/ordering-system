91158d7ab92d08cb723f41c0fa11b61e2fb42a3a
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-11 18:16:54 -0300

    ✨ Adiciona command de Import

---
+++ Orders.Domain/Commands/CreateImportCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Flunt.Notifications;
+using Orders.Domain.Commands.Contracts;
+using Orders.Domain.Entities;
+using Orders.Domain.Enums;
+
+namespace Orders.Domain.Commands
+{
+    public class CreateImportCommand : Notifiable, ICommand
+    {
+        public CreateImportCommand()
+        {
+            Items = new List<CreateImportItemCommand>();
+        }
+        public CreateImportCommand(FileExtension fileExtension, ImportStatus importStatus, IList<CreateImportItemCommand> items)
+        {
+            Instant = DateTime.Now;
+            FileType = fileExtension;
+            ImportStatus = importStatus;
+            Items = items;
+        }
+
+        public DateTime Instant { get; private set; }
+        public ImportStatus ImportStatus { get; private set; }
+        public FileExtension FileType { get; private set; }
+
+        public IList<CreateImportItemCommand> Items { get; private set; }
+        public void Validate()
+        {
+        }
+    }
+}
