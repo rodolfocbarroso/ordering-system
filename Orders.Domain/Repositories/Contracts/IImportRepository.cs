5a24e40b7246633e1f6f912c3ccc041ef7e6ae99
rodolfocbarroso <rodolfoc.barroso@hotmail.com>
2021-02-12 06:36:58 -0300

    🔨 Refatora contract de reposiory de Import

--- Orders.Domain/Repositories/Contracts/IImportRepository.cs
+++ Orders.Domain/Repositories/Contracts/IImportRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Text;
+using System.Threading.Tasks;
 using Orders.Domain.Entities;
 
 namespace Orders.Domain.Repositories.Contracts
@@ -9,8 +10,8 @@ namespace Orders.Domain.Repositories.Contracts
     public interface IImportRepository
     {
         void Save(Import import);
-        Import GetById(Guid id);
-        IEnumerable<Import> GetAll();
+        Task<Import> GetById(Guid id);
+        Task<IEnumerable<dynamic>> GetAll();
 
     }
 }
